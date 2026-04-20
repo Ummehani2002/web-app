08f71702308ff17bd665e7e3182efba195bf282f
Umme Hani <haniumme698@gmail.com>
2026-04-20 15:42:49 +0400

    chnages in deployement for open browser

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,17 @@
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\ItemIssueController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api.bearer')->group(function () {
     Route::post('/customers', [CustomerController::class, 'store']);
     Route::apiResource('/companies', CompanyController::class);
     Route::apiResource('/projects', ProjectController::class);
+    Route::post('/item-issue/items/lookup', [ItemIssueController::class, 'lookupItems'])
+        ->name('api.item-issue.items.lookup');
+    Route::post('/item-issue/projects/lookup', [ItemIssueController::class, 'lookupProjects'])
+        ->name('api.item-issue.projects.lookup');
+    Route::post('/item-issue/post', [ItemIssueController::class, 'post'])
+        ->name('api.item-issue.post');
 });
